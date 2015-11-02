27a8fa836257f732c5e4830a23be10d602e3e23a
Thuan123 <thuanbk56@gmail.com>
2015-11-03 01:59:36 +0700

    Add user following

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Chuong3Sampleapp
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    # Include the authenticity token in remote forms.
+   
+
   end
 end
